e3d1b40e556c657de558bb5e279b026b58488903
Apple-Kong <kongthn@gmail.com>
2023-11-14 23:23:22 +0900

    [Feat] 게임 아이템 카드 컴포넌트 화

--- lib/screen/play/components/game_action_dialog.dart
+++ lib/screen/play/components/game_action_dialog.dart
@@ -6,6 +6,7 @@ import 'package:money_cycle/components/mc_container.dart';
 import 'package:money_cycle/constants.dart';
 import 'package:money_cycle/models/enums/game_action_type.dart';
 import 'package:money_cycle/controller/game_controller.dart';
+import 'package:money_cycle/models/game_action.dart';
 import 'package:money_cycle/screen/play/components/action_choice_button.dart';
 // import 'package:money_cycle/screen/game_play_screen.dart';
 
@@ -237,82 +238,9 @@ class GameActionDialog extends StatelessWidget {
                           itemBuilder: (context, index) {
                             final item = gameController
                                 .curretnSpecificActionModel?.items[index];
-                            return Padding(
-                              padding: const EdgeInsets.only(
-                                  right: 10, top: 1, bottom: 1),
-                              child: Container(
-                                clipBehavior: Clip.antiAlias,
-                                width: 110,
-                                height: 148,
-                                decoration: ShapeDecoration(
-                                  color: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                    side: const BorderSide(
-                                        width: 1,
-                                        color: Colors.white,
-                                        strokeAlign:
-                                            BorderSide.strokeAlignOutside),
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  shadows: const [
-                                    BoxShadow(
-                                      color: Color(0x4C000000),
-                                      blurRadius: 6,
-                                      offset: Offset(3, 3),
-                                      spreadRadius: 1,
-                                    )
-                                  ],
-                                ),
-                                child: Column(children: [
-                                  Container(
-                                    clipBehavior: Clip.hardEdge,
-                                    height: 60,
-                                    decoration: BoxDecoration(
-                                        color: gameController.currentCardColor),
-                                    child: Align(
-                                      alignment: Alignment.bottomLeft,
-                                      child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text(
-                                          item?.title ?? "",
-                                          style: Constants.defaultTextStyle
-                                              .copyWith(fontSize: 16),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 74,
-                                    child: Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 6, horizontal: 8),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            "${item?.price}원",
-                                            style: Constants.defaultTextStyle
-                                                .copyWith(
-                                                    fontSize: 16,
-                                                    color: Constants.dark100),
-                                          ),
-                                          const SizedBox(
-                                            height: 4,
-                                          ),
-                                          Text(
-                                            item?.description ?? "",
-                                            style: Constants.defaultTextStyle
-                                                .copyWith(
-                                                    fontSize: 10,
-                                                    color: Constants.dark100),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  )
-                                ]),
-                              ),
+                            return GameItemCard(
+                              item: item,
+                              accentColor: gameController.currentCardColor,
                             );
                           }),
                     ),
@@ -323,5 +251,87 @@ class GameActionDialog extends StatelessWidget {
         ],
       );
     });
+  }
+}
+
+class GameItemCard extends StatelessWidget {
+  const GameItemCard({
+    super.key,
+    required this.item,
+    this.accentColor,
+  });
+
+  final GameActionItem? item;
+  final Color? accentColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10, top: 1, bottom: 1),
+      child: Container(
+        clipBehavior: Clip.antiAlias,
+        width: 110,
+        height: 148,
+        decoration: ShapeDecoration(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            side: const BorderSide(
+                width: 1,
+                color: Colors.white,
+                strokeAlign: BorderSide.strokeAlignOutside),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          shadows: const [
+            BoxShadow(
+              color: Color(0x4C000000),
+              blurRadius: 6,
+              offset: Offset(3, 3),
+              spreadRadius: 1,
+            )
+          ],
+        ),
+        child: Column(children: [
+          Container(
+            clipBehavior: Clip.hardEdge,
+            height: 60,
+            decoration: BoxDecoration(color: accentColor),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  item?.title ?? "",
+                  style: Constants.defaultTextStyle.copyWith(fontSize: 16),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 74,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "${item?.price}원",
+                    style: Constants.defaultTextStyle
+                        .copyWith(fontSize: 16, color: Constants.dark100),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                    item?.description ?? "",
+                    style: Constants.defaultTextStyle
+                        .copyWith(fontSize: 10, color: Constants.dark100),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
