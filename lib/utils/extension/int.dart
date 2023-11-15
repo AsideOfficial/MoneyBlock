555b355ba36cf3439686bf882e0f35aa5b28fd4f
Apple-Kong <kongthn@gmail.com>
2023-11-15 17:21:44 +0900

    [Refactor] 언유즈드 패키지 제거

--- lib/utils/extension/int.dart
+++ lib/utils/extension/int.dart
@@ -2,7 +2,6 @@ extension IntExtension on int {
   String get commaString {
     String strNumber = toString();
     RegExp regExp = RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
-    Iterable<Match> matches = regExp.allMatches(strNumber);
     String result =
         strNumber.replaceAllMapped(regExp, (Match match) => '${match[1]},');
     return result;
