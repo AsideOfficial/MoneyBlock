06158ebee278a0b69cb385a41983b6058380cb66
Apple-Kong <kongthn@gmail.com>
2023-11-17 15:03:04 +0900

    [Fix] 시작 화면 수정

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:money_cycle/app_pages.dart';
 import 'package:money_cycle/firebase_options.dart';
 import 'package:money_cycle/screen/lobby/screens/lobby_screen.dart';
 import 'package:money_cycle/screen/play/game_play_screen.dart';
+import 'package:money_cycle/start/start_screen.dart';
 
 FirebaseApp? firebaseApp;
 
@@ -44,7 +45,7 @@ class MyApp extends StatelessWidget {
             if (snapshot.hasData) {
               return const LobbyScreen();
             } else {
-              return const GamePlayScreen();
+              return const StartScreen();
             }
           }),
         ),
